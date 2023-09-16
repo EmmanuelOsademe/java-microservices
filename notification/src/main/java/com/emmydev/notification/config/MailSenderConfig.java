edbe7fc5e4fbcf236a3e3c6e13ba9af2f8e8fd75
Emmanuel Osademe <emma.osademe@gmail.com>
2023-09-16 19:51:34 +0100

    Implemented docker networking

--- notification/src/main/java/com/emmydev/notification/config/MailSenderConfig.java
+++ notification/src/main/java/com/emmydev/notification/config/MailSenderConfig.java
@@ -1,28 +1,23 @@
 package com.emmydev.notification.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-import java.util.Objects;
+
 import java.util.Properties;
 
 @Configuration
 public class MailSenderConfig {
-    @Autowired
-    private Environment env;
 
     @Bean
     public JavaMailSender javaMailSender(){
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-
-        mailSender.setHost(env.getProperty("EMAIL_HOST"));
-        mailSender.setPort(Integer.parseInt(Objects.requireNonNull(env.getProperty("EMAIL_PORT"))));
-        mailSender.setUsername(env.getProperty("EMAIL_USERNAME"));
-        mailSender.setPassword(env.getProperty("EMAIL_PASSWORD"));
+        mailSender.setHost("${mail.host}");
+        mailSender.setPort(587);
+        mailSender.setUsername("${mail.username}");
+        mailSender.setPassword("${mail.password}");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
